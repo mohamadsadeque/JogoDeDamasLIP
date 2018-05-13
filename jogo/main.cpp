2b8b5da2d95b7280d3985941d3722494fda5f253
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2018-05-13 20:45:59 -0300

    Update main.cpp

--- jogo/main.cpp
+++ jogo/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
 using namespace std;
+ int modo;
+char x,y,novoX,novoY;
 
+bool jogadaValida(char x, char y, int peca, char tab[8][8]){
+    if( (x >= 'a' && x <= 'h') && (y >= '1' && y <= '8' ) ){
+        if(tab[y-49][x-97] == 'x' && peca == 1){
+            return true;
+        }
+        else if(tab[y-49][x-97] == 'o' && peca == 2){
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
 
+}
+
+void jogador(char x, char y, char novoX , char novoY , char tab[8][8] ){
+        tab[novoY-49][novoX-97] = tab[y-49][x-97];
+        tab[y-49][x-97] = ' ';
+
+}
 void ImprimeTabuleiro(char tab[8][8]){
     cout << "          a   b   c   d   e   f   g   h" << endl;
     cout << "        |---|---|---|---|---|---|---|---|" << endl;
@@ -40,7 +62,62 @@ char tab[8][8]{
 
 int main()
 {
-    ImprimeTabuleiro(tab);
+
+    cout << "Digite a opção que deseja: " << endl;
+    cout << "1. Jogador Vs Jogador" << endl;
+    cout << "2. Jogador Vs Computador" << endl;
+    cin >> modo;
+    while(modo != 1 && modo != 2){  cout << "Jogador 1 (x) , digite a peca que deseja mover (exemplo: b 3)" << endl;
+        cin >> x >> y;
+        while(!jogadaValida(x,y,1, tab)){
+            cout << "JOGADA INVALIDA, ESCOLHA NOVAMENTE A PECA QUE DESEJA MOVER: " << endl;
+            cin >> x >> y;
+        }
+        cout << "Jogador 1 (x) , digite para onde peca deve ser movida (exemplo: b 3)" << endl;
+        cin >> novoX >> novoY;
+        jogador(x,y,novoX,novoY,tab);
+        ImprimeTabuleiro(tab);
+        cout << "Modo invalido, digite novamente: "<< endl;
+        cin >> modo;
+    }
+    switch (modo){
+    case 1:
+        ImprimeTabuleiro(tab);
+        cout << "Jogador 1 (x) , digite a peca que deseja mover (exemplo: b 3)" << endl;
+        cin >> x >> y;
+        while(!jogadaValida(x,y,1, tab)){
+            cout << "JOGADA INVALIDA, ESCOLHA NOVAMENTE A PECA QUE DESEJA MOVER: " << endl;
+            cin >> x >> y;
+        }
+        cout << "Jogador 1 (x) , digite para onde peca deve ser movida (exemplo: b 3)" << endl;
+        cin >> novoX >> novoY;
+        jogador(x,y,novoX,novoY,tab);
+        ImprimeTabuleiro(tab);
+        cout << "Jogador 2 (o) , digite a peca que deseja mover (exemplo: b 3)" << endl;
+        cin >> x >> y;
+        while(!jogadaValida(x,y,2, tab)){
+            cout << "JOGADA INVALIDA, ESCOLHA NOVAMENTE A PECA QUE DESEJA MOVER: " << endl;
+            cin >> x >> y;
+        }
+        cout << "Jogador 2 (o) , digite para onde peca deve ser movida (exemplo: b 3)" << endl;
+        cin >> novoX >> novoY;
+        jogador(x,y,novoX,novoY,tab);
+        ImprimeTabuleiro(tab);
+
+
+        break;
+    case 2:
+        ImprimeTabuleiro(tab);
+        while(1){
+            cin >> x >> y;
+            tab[y-49][x-97] = 'A';
+            ImprimeTabuleiro(tab);
+
+        }
+        break;
+
+
+    }
 
     return 0;
 }
